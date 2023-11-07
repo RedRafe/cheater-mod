a29a130ef9dfbe5a95a4919fe7c85f0e17fe2b14
RedRafe <gabri97.gt+redrafe@gmail.com>
2023-11-07 23:24:31 +0100

    v0.1.0

---
+++ data-updates.lua
@@ -0,0 +1,49 @@
+local speaker = data.raw['programmable-speaker']['programmable-speaker']
+
+speaker.instruments[#speaker.instruments+1] = {
+	name = 'meme_sounds',
+	notes = {
+		{ 
+      name = 'dewit',
+      sound = {
+        filename = '__cheater-mod__/sound/dewit.ogg'
+      }
+    },
+    { 
+      name = 'garfield',
+      sound = {
+        filename = '__cheater-mod__/sound/garfield.ogg'
+      }
+    },
+    { 
+      name = 'kekw',
+      sound = {
+        filename = '__cheater-mod__/sound/kekw.ogg'
+      }
+    },
+    { 
+      name = 'look-at-this-dude',
+      sound = {
+        filename = '__cheater-mod__/sound/look-at-this-dude.ogg'
+      }
+    },
+    { 
+      name = 'oh-no',
+      sound = {
+        filename = '__cheater-mod__/sound/oh-no.ogg'
+      }
+    },
+    { 
+      name = 'space',
+      sound = {
+        filename = '__cheater-mod__/sound/space.ogg'
+      }
+    },
+    { 
+      name = 'stupid',
+      sound = {
+        filename = '__cheater-mod__/sound/stupid.ogg'
+      }
+    },
+  }
+}
